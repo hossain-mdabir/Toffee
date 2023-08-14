225050645c6f451b17b19ad7c1f8d591f87f3c64
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-08-14 11:17:09 +0600

    Initial Commit

---
+++ Toffee/ToffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToffeeApp.swift
+//  Toffee
+//
+//  Created by Md Abir Hossain on 14-08-2023.
+//
+
+import SwiftUI
+
+@main
+struct ToffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
